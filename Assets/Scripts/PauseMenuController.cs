837291a277fd3d78a4267c212bffa4103478064b
Alexis Jin <xj606@nyu.edu>
2024-06-20 14:37:36 -0400

    UI

---
+++ Assets/Scripts/PauseMenuController.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuController : MonoBehaviour
+{
+    public GameObject pauseMenuPanel;
+    private bool isPaused = false;
+
+    void Update()
+    {
+        // Toggle pause menu on Tab key press
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        // Resume the game
+        pauseMenuPanel.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void RestartGame()
+    {
+        // Restart the game
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void ExitGame()
+    {
+        // Exit to start menu
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("StartMenu");
+    }
+
+    private void PauseGame()
+    {
+        // Pause the game
+        pauseMenuPanel.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+}
---
+++ Assets/Scripts/StartMenuController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuController : MonoBehaviour
+{
+    public GameObject startMenuPanel;
+    public GameObject settingMenuPanel;
+
+    public void StartGame()
+    {
+        // Load the main game scene
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void OpenSettings()
+    {
+        // Open the settings menu
+        startMenuPanel.SetActive(false);
+        settingMenuPanel.SetActive(true);
+    }
+
+    public void BackToStartMenu()
+    {
+        // Return to the start menu from the settings menu
+        settingMenuPanel.SetActive(false);
+        startMenuPanel.SetActive(true);
+    }
+
+    public void ExitGame()
+    {
+        // Exit the game
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
+    }
+}
